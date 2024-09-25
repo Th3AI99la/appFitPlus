fc6430064fd6c0eb7f23eb0df7c0dbbaf62fdfcf
Th3AI99la <talles.th54@gmail.com>
2024-09-25 01:15:59 -0300

    mobile - version 1.0.2

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,72 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "../constants/colors";
 
 export default function Index() {
   return (
-    <View>
-      <Image source={require('../assets/images/logo.png')} />
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo5.png")} />
+
+      <Text style={styles.title}>
+        FitPlus<Text style={{ color: colors.white }}>.AI</Text>
+      </Text>
+
+      <Text style={styles.text}>
+        Seu aplicativo de dieta inteligente para uma vida mais saudável e equilibrada.
+      </Text>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar Dieta</Text>
+      </Pressable>
     </View>
   );
 }
+
+// Criar grupos de estilo personalizados
+const styles = StyleSheet.create({
+  //#container
+  container: {
+    //verde
+    backgroundColor: colors.background,
+    // flex, tela inteira
+    flex: 1,
+    //centralizar
+    justifyContent: "center",
+    // alinhar horizontamente
+    alignItems: "center",
+    //espaçamento interno (NÃO GRUDAR NA BORDA DO CELULAR)
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  //#title
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: colors.yellow
+  },
+  //#text
+  text: {
+    fontSize: 15,
+    color: colors.white,
+    width: 261, // altere conforme necessario (texto)
+    textAlign: "center",
+    marginTop: 8,
+    marginBottom: 8
+  },
+  //#button
+  button: {
+    backgroundColor: colors.blue,
+    width: "100%",
+    height: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 35 // desgrudar
+  },
+
+  //#buttonText (Texto do botão)
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: "bold"
+  }
+});
