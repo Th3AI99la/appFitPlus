a3ee61e176c7b74d2649c804a13c8564aaf0ea59
Th3AI99la <talles.th54@gmail.com>
2025-02-22 14:06:17 -0300

    SKD - Update

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,3 +1,8 @@
+// Sempre alterar de acordo com o IP do servidor que está rodando o backend
+// Para saber o IP do servidor, basta digitar no terminal o comando: ipconfig
+// E procurar pelo IPV4
+// Alterar o IP no baseURL
+
 import axios from "axios";
 
 // API - conectada ao IPV4 - http://192.168.1.103:8888 /create
@@ -5,5 +10,5 @@ import axios from "axios";
 // http://10.200.2.228:8888 - UEG
 
 export const api = axios.create({
-   baseURL: "http://192.168.100.18:8888"
+   baseURL: "http://192.168.0.211:8888"
 });
