4d066b21fa689c0693028252a17e3d9ea01b7301
Th3AI99la <talles.th54@gmail.com>
2025-05-01 11:35:43 -0300

    Versao 2.1

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,8 +1,3 @@
-// Sempre alterar de acordo com o IP do servidor que está rodando o backend
-// Para saber o IP do servidor, basta digitar no terminal o comando: ipconfig
-// E procurar pelo IPV4
-// Alterar o IP no baseURL
-
 import axios from "axios";
 
 // API - conectada ao IPV4 - http://192.168.1.103:8888 /create
@@ -10,7 +5,5 @@ import axios from "axios";
 // http://10.200.2.228:8888 - UEG
 
 export const api = axios.create({
-   baseURL: "http://192.168.15.90:8888"
+   baseURL: "http://192.168.100.18:8888"
 });
-
-// verificar esse erro do package.json
