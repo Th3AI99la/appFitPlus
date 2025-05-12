fd244e68acff583899e7a854815915d5584781bf
Th3AI99la <talles.th54@gmail.com>
2025-05-12 18:02:49 -0300

    3.1.1

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 // API - conectada ao IPV4 - http://192.168.1.103:8888 /create
 
 export const api = axios.create({
-   baseURL: "http://192.168.15.133:3333"
+   baseURL: "http://192.168.15.133:8888"
 });
